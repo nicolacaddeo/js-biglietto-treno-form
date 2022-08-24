d7a161bedc1d8ba994c721562d5a8b1df0e16b88
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-08-24 16:30:31 +0200

    added calc for total price

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 // variabili
 
-const distance = document.getElementById('Km')
-const userAge = document.getElementById('age')
-const formButton = document.getElementById('formButton')
+let distanceInput = document.getElementById('Km');
+let userAgeInput = document.getElementById('age');
+let formButton = document.getElementById('formButton');
+let pInput = document.getElementById('total-price');
 
 // evento click
+
+formButton.addEventListener('click',
+    function () {
+        let kmTrip = distanceInput.value;
+        let userAge = userAgeInput.value;
+        // controllo dati
+
+        if (!isNaN(kmTrip) && !isNaN(userAge)) {
+            console.log('Hai inserito i dati correttamente');
+            let totalPrice = (kmTrip * 0.21);
+            let discount = 0;
+            if (userAge < 18) {
+                discount = (totalPrice / 100 * 20);
+            } else if (age > 65) {
+                discount = (totalPrice / 100 * 40);
+            }
+            totalPrice = totalPrice - discount;
+            console.log(`Il tuo biglietto costa ${totalPrice.toFixed(2)} euro`);
+        } else {
+            console.log('Puoi inserire solo numeri');
+        }
+    }
+);
+
+
+
+
+
+
+
